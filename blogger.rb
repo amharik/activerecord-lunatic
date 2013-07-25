4e5073d214254ceccf784bbcb9ffd11b727a4bd1
Harikrishnan <harikrishnan.a@infibeam.net>
2013-07-25 16:50:17 +0530

    Complete active record model querying API. needs Testing

--- blogger.rb
+++ blogger.rb
@@ -2,7 +2,6 @@ require './require_libs.rb'
 
 class Blogger
   def self.method_missing method, *args, &block
-#     super unless method =~ /[a-z]+_[inby0-9]+_[a-z0-9_]+/
     super unless method =~ /[a-z]+_[a-z0-9_]+/
     case method
     when /total_[a-z0-9]+/
@@ -33,30 +32,3 @@ class Blogger
     end
   end
 end
-
-Blogger.last_2_users.each{|u| puts u.name}
-Blogger.last_2_blogs.each{|b| puts b.title}
-Blogger.last_n_posts.each{|p| puts p.content}
-
-Blogger.first_1_comments.each{|c| puts c.content}
-
-Blogger.posts_in_blog.each{|b| b.posts.each{|p| puts p.content}}
-
-Blogger.comments_by_user.each{|u| u.comments.each{|c| puts c.content}}
-
-Blogger.blogs_by_date_2013_7_25.each{|b| puts b}
-
-puts "total number of blogs is #{Blogger.total_blogs}"
-puts "total number of users is #{Blogger.total_users}"
-
-#IMP
-# 
-#     when /[a-z]+_by_date_[0-9]+/
-#       table,date = method.to_s.split "_by_date_"
-#        puts "finding all "+table+" at "+date
-#       return
-
-#posts in a blog
-#comment user
-#group month
-#tot blogs
