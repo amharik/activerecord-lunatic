b4694a0bb75a35fa99f95e25eb2fa8e633be0bec
Harikrishnan <harikrishnan.a@infibeam.net>
2013-07-26 11:51:31 +0530

    Added grouping blogs by month-year

--- blogger.rb
+++ blogger.rb
@@ -8,23 +8,21 @@ class Blogger
       emptyString,table = method.to_s.split "total_"
       return eval "#{table.capitalize.singularize}.count;"
       
+    when /group_blogs/
+      return Blog.group("DATE_FORMAT(created_at, '%m-%y')").count('*')
+      
     when /[a-z]+_[n0-9]+_[a-z]+/
       position,count,table= method.to_s.split "_"
       return (count=="n")? (eval "#{table.capitalize.singularize}.all;") :(eval "#{table.capitalize.singularize}.#{position}(#{count});")
       
-    when /[a-z]+_by_date_[0-9]+/
-      table,date = method.to_s.split "_by_date_"
-      puts "finding all "+table+" at "+date
-      date=date.split "_"
-      condition = ""
-      (date.length>1 && condition+="YEAR(created_at)=? ")?((date.length>2 && condition+="and MONTH(created_at)=? ")?((date.length>3)?(condition+="and DAY(created_at)=? "):(condition+="and DAY(created_at)=? ")):condition+="and MONTH(created_at)=? "):(condition="YEAR(created_at)=? ");
-      #       SELECT `blogs`.* FROM `blogs` WHERE (YEAR(created_at)=2013 and MONTH(created_at)=7 and DAY(created_at)=25 )
-      return eval "#{table.capitalize.singularize}.where('#{condition}',#{date[0..2].join ","})";
+    when /[a-z]+_by_date/
+      table,empty= method.to_s.split "_by_date"
+      hash=args[0]
+      return eval "#{table.capitalize.singularize}.where(\"DATE_FORMAT(created_at,'%m-%y')='#{hash[:month]}-#{hash[:year]}'\")";
       
     when /[a-z]+_(in|by)_[a-z]+/
       child,relation,parent= method.to_s.split "_"
       puts "finding "+child+" in "+parent
-      #     ELIMINATES N+1 PROBLEM  Blog.includes(:posts).all
       return (eval "#{parent.capitalize}.includes(:#{child}).all");
       
     else
--- playground.rb
+++ playground.rb
@@ -3,20 +3,17 @@ require './blogger.rb'
 Blogger.last_2_users.each{|u| puts u.name}
 Blogger.last_2_blogs.each{|b| puts b.title}
 Blogger.last_n_posts.each{|p| puts p.content}
-
 Blogger.first_1_comments.each{|c| puts c.content}
 
 Blogger.posts_in_blog.each{|b| b.posts.each{|p| puts p.content}}
 
 Blogger.comments_by_user.each{|u| u.comments.each{|c| puts c.content}}
 
-Blogger.blogs_by_date_2013_7_25.each{|b| puts b}
+# groups blog by "month-year"
+puts "The number of blogs created at 07-13 is "
+puts Blogger.group_blogs["07-13"]
+puts "The Blogs are:"
+(Blogger.blogs_by_date year:'13',month:'07').each_with_index{|b,i|puts "#{i+1}. #{b.title}"}
 
 puts "total number of blogs is #{Blogger.total_blogs}"
 puts "total number of users is #{Blogger.total_users}"
-
-#REQUIREMENTS
-#posts in a blog
-#comment user
-#group month
-#tot blogs
